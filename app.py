d2e22a8114813d5d2523b1aa296275102cf39d87
Nicolas Desdames <90900993+doctorvonunformlichkeit@users.noreply.github.com>
2024-04-28 13:21:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 from flask import Flask
+app = Flask(__name__)
+
+import sente
+import random
+
+@app.route('/request_ply')
+def request_ply():
+  game = request.args.get('game')
+
+
+  while True:
+    a=random.randint(1, 19)
+    b=random.randint(1, 19)
+    try:
+      game.play(a, b)
+      return {"ply":"ply"}
+    except:
+      pass
+  
+  
+
+if __name__ == "__main__":
+    app.run()
